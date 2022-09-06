9592134480eefb907f44c74df67b678a7b780fe2
TASMIAD HASAN <107355190+tasmiadhasan@users.noreply.github.com>
2022-09-06 17:35:16 +0600

    Update Coin Management System.c

--- Coin Management System.c
+++ Coin Management System.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 #include "splash.h"
 
 int main(void)
 {
 splash();
+FILE*fptr;char u[50],p[50];
+char user[100];
+char pass[100];
+char file[100];
+file[100] = "pass.txt";
+printf("Enter your user name:");
+scanf("%c",&u);
+fptr=fopen(file,"r");
+fgets(user,size)
+
+
+fptr=fopen("pass.txt","r");
+
+
 
 return 0;
 }
